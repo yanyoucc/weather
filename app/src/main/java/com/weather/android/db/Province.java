ab61b496591d8f5935eb36f69a24147e1a7baee3
yanyoucc <yanyoucc@gmail.com>
2018-07-29 12:46:49 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/weather/android/db/Province.java
@@ -0,0 +1,31 @@
+package com.weather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by yanyo on 2018/7/29.
+ */
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getProvinceNmae() {
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
+
